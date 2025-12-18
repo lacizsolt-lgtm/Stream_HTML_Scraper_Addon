d2ba9ab72e425902571c8a9ae09e102fac137b23
lacizsolt-lgtm <lacizsolt@gmail.com>
2025-12-18 17:50:42 +0100

    Update index.js

--- URLresolver/index.js
+++ URLresolver/index.js
@@ -2,20 +2,12 @@ const videa = require('./videa');
 
 const universalResolver = {
     resolve: async (url) => {
-        if (!url) return null;
-
-        // Ellenőrizzük, hogy Videa link-e
-        if (url.includes('videa.hu') || url.includes('videakid.hu')) {
-            // Itt a videa.js-ben lévő resolve függvényt hívjuk meg
+        if (url && (url.includes('videa.hu') || url.includes('videakid.hu'))) {
+            // A videa.js-ből importált objektum 'resolve' metódusát hívjuk
             return await videa.resolve(url);
         }
-        
-    // Ide jöhetnek majd a többiek:
-    // if (url.includes('mixdrop.co')) return await mixdropResolve(url);
-        
-        // Ha nem ismerjük, visszaadjuk az eredetit (vagy null-t)
-        return url;
+        return null;
     }
 };
 
-module.exports = { resolve: scrapeVidea };
+module.exports = { universalResolver };
